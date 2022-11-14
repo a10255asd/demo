35e9ddbb834ce644ab267f883f84f3d0f0713514
刘鸡血 <1025519998@qq.com>
2022-11-14 16:47:44 +0800

    1114commit

--- src/main/java/com/example/leetCdoe/easy/leetcode108.java
+++ src/main/java/com/example/leetCdoe/easy/leetcode108.java
@@ -10,4 +10,17 @@ package com.example.leetCdoe.easy;
  */
 public class leetcode108 {
     //108、将有序数组转换为二叉搜索树
+    public TreeNode sortedArrayToBST(int[] nums) {
+       return helper(nums,0,nums.length-1);
+    }
+    public TreeNode helper(int[] nums,int left,int right){
+        if(left > right){
+            return null;
+        }
+        int mid = left + (right - left) /2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums,left,mid-1);
+        root.right = helper(nums,mid+1,right);
+        return root;
+    }
 }
